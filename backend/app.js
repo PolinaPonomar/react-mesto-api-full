763f0468800340d69c04e36ad9c90a8448f29383
Polina Ponomareva <pp09117@gmail.com>
2021-05-20 16:08:41 +0300

    feat: дописать cors

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,11 @@ app.use(helmet());
 // парсинг данных
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://mesto.polina.ponomareva.nomoredomains.icu',
+    'https://mesto.polina.ponomareva.nomoredomains.icu'],
+  // 'http://localhost:3000'],
+}));
 
 // подключаем логгер запросов
 app.use(requestLogger);
